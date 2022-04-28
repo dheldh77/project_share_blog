6320a6e255dddc64a89eb3e069489acec1b1c7b2
jyaquinas <jythomaskim@gmail.com>
2022-04-28 22:19:18 +0900

    [FEAT] Add profile controller for zero downtime deployment
    Test code for profile controller included

---
+++ src/test/java/com/example/ShareBlog/web/ProfileControllerTest.java
@@ -0,0 +1,36 @@
+package com.example.ShareBlog.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProfileControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void call_profile(){
+        String expected = "default";
+
+        ResponseEntity<String> response = restTemplate.getForEntity("/profile",
+                String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(expected);
+    }
+}
