8645fc10e481b773995439d155a2f359685d7664
jyaquinas <jythomaskim@gmail.com>
2022-05-05 22:54:46 +0900

    [FEAT] Add user entity class and repository

--- src/main/java/com/example/ShareBlog/domain/user/User.java
+++ src/main/java/com/example/ShareBlog/domain/user/User.java
@@ -1,5 +1,52 @@
 package com.example.ShareBlog.domain.user;
 
-public class User {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.cassandra.core.cql.Ordering;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.Table;
+import org.springframework.data.domain.Persistable;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor
+@Table(value = "user_by_id")
+public class User implements Persistable<UUID> {
+    @Id
+    @PrimaryKeyColumn(name = "post_id", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
+    private UUID id;
+
+    @PrimaryKeyColumn(ordinal = 1, ordering = Ordering.ASCENDING)
+    private String lastName;
+
+    @PrimaryKeyColumn(ordinal = 2, ordering = Ordering.ASCENDING)
+    private String firstName;
+
+    private String username;
+
+    @CreatedDate
+    private LocalDateTime dateCreated;
+
+    @LastModifiedDate
+    private LocalDateTime dateModified;
+
+    @Transient
+    private boolean isNew;
+
+    @Override
+    public boolean isNew() {
+        if (isNew) {
+            isNew = false;
+            return true;
+        }
+        return false;
+    }
 
 }
---
+++ src/main/java/com/example/ShareBlog/domain/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.ShareBlog.domain.user;
+
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends CassandraRepository<User, UUID> {
+}
